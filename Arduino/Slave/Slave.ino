724a5d93346fe37c59074cad504ef14fc3ccccd2
Owen Hancock <tshancock2727@gmail.com>
2025-05-12 14:43:40 -0500

    Adding updated slave file from Alexia

--- Arduino/Slave/Slave.ino
+++ Arduino/Slave/Slave.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 #include <LiquidCrystal.h>
+#include <PID_v1.h>
+
 
 // LCD and Distance Sensor Setup
 LiquidCrystal lcd(6, 12, 5, 4, 3, 2);
@@ -19,20 +21,30 @@ VL53L0X sensor3;
 #define XSHUT_4 7 // To temporarily shut it off after init (pin 7)
 #define CSensor_Pin A0
 
-// Distance Mapping Parameters
-const int MIN_DIST = 50;   // Minimum distance (mm)
-const int MAX_DIST = 300;  // Maximum distance (mm)
-const int MAX_PWM = 255;   // Max PWM value
+// // Distance Mapping Parameters
+// const int MIN_DIST = 50;   // Minimum distance (mm)
+// const int MAX_DIST = 300;  // Maximum distance (mm)
+// const int MAX_PWM = 255;   // Max PWM value
 
 const float vcc = 5;  // Reference voltage (12V power supply)
+
+// Target distance in mm
+const double Setpoint = 191;
 
 // Variables to track system state
 int distance1 = 0;
 int distance2 = 0;
-int distance3 = 0;
+// int distance3 = 0;
 //int distance4 = 0;
-int avgDistance = 0;
-int pwmValue = 0;
+// int avgDistance = 0;
+// double pwm = 0;
+double avgDistance = 0;
+double Output = 0;
+double pwm = 0;
+double Input = 0;
+double error = 0;
+double previousError = 0;
+double integral = 0;
 bool buzzerActive = false;
 String motorDirection = "IDLE";
 
@@ -40,15 +52,28 @@ String motorDirection = "IDLE";
 unsigned long lastTransmitTime = 0;
 const int TRANSMIT_INTERVAL = 100; // Transmit data every 100ms
 
+// PID gains - adjust as needed
+double Kp = 8.9507;
+double Ki = 1024.6145;//224.6145;
+double Kd = 0.08917;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
 void setup() {
   Serial.begin(9600);
+
+  // Set up motor pins
+  pinMode(MOTOR_CW, OUTPUT);
+  pinMode(MOTOR_CCW, OUTPUT);
+  analogWrite(MOTOR_CW, 205);
+  analogWrite(MOTOR_CCW, 0);
+
   Wire.begin();
   lcd.begin(16, 2); // Initialize LCD (16x2)
 
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(XSHUT_1, OUTPUT);
   pinMode(XSHUT_2, OUTPUT);
-  pinMode(XSHUT_3, OUTPUT);
+  // pinMode(XSHUT_3, OUTPUT);
   //pinMode(XSHUT_4, OUTPUT);
 
   lcd.clear();
@@ -56,16 +81,23 @@ void setup() {
   
   // Initialize all sensors
   initializeSensors();
+
+  sensor1.startContinuous();
+  sensor2.startContinuous();
+  // sensor3.startContinuous();
   
-  // Set up motor pins
-  pinMode(MOTOR_CW, OUTPUT);
-  pinMode(MOTOR_CCW, OUTPUT);
-  analogWrite(MOTOR_CW, 0);
-  analogWrite(MOTOR_CCW, 0);
+  // // Set up motor pins
+  // pinMode(MOTOR_CW, OUTPUT);
+  // pinMode(MOTOR_CCW, OUTPUT);
+  // analogWrite(MOTOR_CW, 205);
+  // analogWrite(MOTOR_CCW, 0);
   
   lcd.clear();
   lcd.print("System Ready");
   delay(1000);
+  
+  myPID.SetMode(AUTOMATIC);
+
 }
 
 void loop() {
@@ -117,16 +149,16 @@ void initializeSensors() {
     while (1) {}
   }
   
-  // Initialize Sensor 3
-  digitalWrite(XSHUT_3, HIGH);
-  delay(100);
-  sensor3.setAddress(0x18);
-  sensor3.setTimeout(500);
-  if (!sensor3.init()) {
-    lcd.clear();
-    lcd.print("Sensor 3 Failed!");
-    while (1) {}
-  }
+  // // Initialize Sensor 3
+  // digitalWrite(XSHUT_3, HIGH);
+  // delay(100);
+  // sensor3.setAddress(0x18);
+  // sensor3.setTimeout(500);
+  // if (!sensor3.init()) {
+  //   lcd.clear();
+  //   lcd.print("Sensor 3 Failed!");
+  //   while (1) {}
+  // }
   
   // Initialize Sensor 4
   // digitalWrite(XSHUT_4, HIGH);
@@ -145,54 +177,57 @@ void initializeSensors() {
 }
 
 void readSensors() {
-  distance1 = sensor1.readRangeSingleMillimeters();
-  distance2 = sensor2.readRangeSingleMillimeters();
-  distance3 = sensor3.readRangeSingleMillimeters();
+  distance1 = sensor1.readRangeContinuousMillimeters();
+  distance2 = sensor2.readRangeContinuousMillimeters();
+  // distance3 = sensor3.readRangeContinuousMillimeters();
   //distance4 = sensor4.readRangeSingleMillimeters();
   
   // Handle timeouts but ensure we still send data
-  if (sensor1.timeoutOccurred() || sensor2.timeoutOccurred() || 
-      sensor3.timeoutOccurred() /*|| sensor4.timeoutOccurred()*/) {
+  if (sensor1.timeoutOccurred() || sensor2.timeoutOccurred() /*|| 
+      sensor3.timeoutOccurred() || sensor4.timeoutOccurred()*/) {
     lcd.clear();
     lcd.print("Sensor Timeout!");
     // Instead of returning, we'll continue with whatever data we have
   }
   
   // Compute average distance (even with timeouts)
-  avgDistance = (distance1 + distance2 + distance3 /*+ distance4*/) / 3;
+  avgDistance = (distance1 + distance2 /*+ distance3 + distance4*/) / 2;
 }
 
-void processData() {
-  // Set PWM to maximum value for testing
-<<<<<<< HEAD
-  pwmValue = MAX_PWM; // Force to maximum (255)
-=======
-  pwmValue = 169; // Force to maximum (255)
->>>>>>> dd1cc18fb08b17d278829589b45f583ad9b38811
-  
-  // Choose which direction you want to test
-  // Option 1: Test forward direction only
-  // motorDirection = "FWD";
-  // analogWrite(MOTOR_CW, pwmValue);
-  // analogWrite(MOTOR_CCW, 0);
-  
-  // Option 2 (comment out Option 1 if using this): Test reverse direction only
-  motorDirection = "REV";
-  analogWrite(MOTOR_CW, 0);
-  analogWrite(MOTOR_CCW, pwmValue);
-  
-  // Optionally disable the buzzer for testing
-  buzzerActive = false;
-  noTone(BUZZER_PIN);
-}
-
-<<<<<<< HEAD
-// void processData() { // uncomment this to utilize PWM stress testing
-=======
-// Old Control Code
+  // Input = avgDistance;
+  // myPID.SetOutputLimits(50, 165); 
+  // myPID.Compute();
+  // pwm = map(Output, 50, 165, 205, 90);
+  // analogWrite(MOTOR_CW, pwm);
 
 // void processData() {
->>>>>>> dd1cc18fb08b17d278829589b45f583ad9b38811
+//   // Set PWM to maximum value for testing
+//   pwmValue = 169; // Force to maximum (255)
+  
+//   // Choose which direction you want to test
+//   // Option 1: Test forward direction only
+//   // motorDirection = "FWD";
+//   // analogWrite(MOTOR_CW, pwmValue);
+//   // analogWrite(MOTOR_CCW, 0);
+  
+//   // Option 2 (comment out Option 1 if using this): Test reverse direction only
+//   motorDirection = "REV";
+//   analogWrite(MOTOR_CW, 0);
+//   analogWrite(MOTOR_CCW, pwmValue);
+  
+//   // Optionally disable the buzzer for testing
+//   buzzerActive = false;
+//   noTone(BUZZER_PIN);
+// }
+
+// Old Control Code
+
+void processData() {
+  Input = avgDistance;
+  myPID.SetOutputLimits(50, 165); 
+  myPID.Compute();
+  pwm = map(Output, 50, 165, 205, 90);
+  analogWrite(MOTOR_CW, pwm);
 //   // Map distance to PWM (closer = higher PWM)
 //   pwmValue = map(avgDistance, MIN_DIST, MAX_DIST, MAX_PWM, 0);
 //   pwmValue = constrain(pwmValue, 0, MAX_PWM);
@@ -206,7 +241,7 @@ void processData() {
 //     motorDirection = "REV";
 //     analogWrite(MOTOR_CW, 0);
 //     analogWrite(MOTOR_CCW, pwmValue);
-//   }
+  }
   
 //   // Buzzer control
 //   if (avgDistance <= 60) {
@@ -228,7 +263,7 @@ void updateLocalDisplay() {
   
   lcd.setCursor(0, 1);
   lcd.print("PWM: ");
-  lcd.print(pwmValue);
+  lcd.print(255-pwm);
   if (buzzerActive) {
     lcd.print(" ALERT!");
   }
@@ -247,10 +282,10 @@ void transmitData() {
   
   String dataPacket = String(distance1) + "," +
                      String(distance2) + "," +
-                     String(distance3) + "," +
+                     "0" /*String(distance3)*/ + "," +
                      "0"/*String(distance4)*/ + "," +
                      String(avgDistance) + "," +
-                     String(pwmValue) + "," +
+                     String(pwm) + "," +
                      motorDirection + "," +
                      String(buzzerActive ? 1 : 0) + ";";
   
